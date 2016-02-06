de9dfc286c9752ee5c0419769065e9a8ae593a6c
Daniel Yim <danielyim@gmail.com>
2016-02-05 23:47:12 -0600

    updating sample

--- pinger/configs.sample.js
+++ pinger/configs.sample.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var configs = {
-  PING_INTERVAL: 1000 * 60 * 60, // ms * s * h every hour
+  PING_INTERVAL: 1000 * 60 * 60, // ms * s * h = every hour
   IP_PROVIDER_URL: 'http://canihazip.com/s',
-  DEST_URL: 'https://[your Heroku URL].com'
+  DEST_URL: 'https://<your heroku app name>.herokuapp.com'
 };
 
 module.exports = configs;
