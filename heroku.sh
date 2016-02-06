b74ba2ac0644b948d4035c2209ab707d3befacf7
Daniel Yim <danielyim@gmail.com>
2016-02-05 22:52:28 -0600

    added another command in case the first subtree command doesn't work

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-
+# Only pushes the agent directory to Heroku, which is convenient for multi-app repos like pingy
 git subtree push --prefix agent heroku master
+
+# Also worth a shot if the above doesn't work..
+# Credit: http://stackoverflow.com/a/15623469/350951
+#git push heroku `git subtree split --prefix agent master`:master --force
